c9baca520cf6b901216b8f376c91a71b1f3abb3a
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-12-01 14:35:47 -0200

    [ADD] Simple grid movement added

---
+++ Assets/GridMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour {
+
+	public void MoveBy(Vector3 portion){
+		Vector2 finalPos = transform.position + portion;
+
+		// Verifica se ha algo
+		if(!HaParede(finalPos)){
+			Move(finalPos);
+		}
+	}
+
+	bool HaParede(Vector2 pos){
+		return false;
+	}
+
+	void Move(Vector2 final){
+		// Do animation
+		
+		// Change transform position
+		transform.position = final;
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GridMovement))]
+public class Player : MonoBehaviour {
+
+	public float cooldown = 0.5f;
+	private float movementCooldown;
+	private GridMovement _gridMovement;
+
+	// Use this for initialization
+	void Start () {
+		_gridMovement = GetComponent<GridMovement>();
+		movementCooldown = cooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movementCooldown -= Time.deltaTime;
+		if(movementCooldown <= 0){
+			if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
+				movementCooldown = cooldown;
+				_gridMovement.MoveBy(Vector2.left);
+				return;
+			}
+
+			if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
+				movementCooldown = cooldown;
+				_gridMovement.MoveBy(Vector2.right);
+				return;
+			}
+
+			if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
+				movementCooldown = cooldown;
+				_gridMovement.MoveBy(Vector2.up);
+				return;
+			}
+
+			if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
+				movementCooldown = cooldown;
+				_gridMovement.MoveBy(Vector2.down);
+				return;
+			}
+		}
+	}
+}
