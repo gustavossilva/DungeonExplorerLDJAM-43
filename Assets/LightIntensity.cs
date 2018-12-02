3756c2995e7047443b03b5993a83d237d2249bec
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-12-02 01:38:12 -0200

    Adiciona cenario iluminado ; cria as tiles do cenario ; coloca o jogador na tela principal ; cria os colisores das paredes ; primeira versao da camera

---
+++ Assets/LightIntensity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightIntensity : MonoBehaviour {
+
+	public Light _lightPoint;
+	[SerializeField]private float maxFireIntensity,minFireIntensity;
+	// Use this for initialization
+	void Start () {
+		_lightPoint = GetComponent<Light>();
+		StartCoroutine ( FireIntensity());
+	}
+
+	private void Update() {
+       
+	}
+
+	IEnumerator FireIntensity(){
+		while(true){
+			yield return new WaitForEndOfFrame();
+			_lightPoint.intensity = Random.Range(minFireIntensity,maxFireIntensity);
+		}
+	}
+}
---
+++ Assets/Scripts/MonsterBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBehaviour : MonoBehaviour {
+	public float cooldown = 0.35f;
+	private float movementCooldown;
+	private GridMovement _gridMovement;
+	[SerializeField] private MonsterType _monsterType;
+
+	private Vector3 _startPosition;
+	// Use this for initialization
+	void Start () {
+		_gridMovement = GetComponent<GridMovement> ();
+		movementCooldown = cooldown;
+		_startPosition = this.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		movementCooldown -= Time.deltaTime;
+		if (movementCooldown <= 0) {
+			switch (_monsterType) {
+				case MonsterType.SLIME:
+					//Dispara animação de movimento
+					if(this.transform.position != _startPosition)
+						_gridMovement.MoveBy (Vector2.up);
+					else
+						_gridMovement.MoveBy (Vector2.down);
+					break;
+				case MonsterType.GOBLIN:
+					break;
+				case MonsterType.SKELETON:
+					break;
+				case MonsterType.TROLL:
+					break;
+				default:
+					break;
+			}
+			movementCooldown = cooldown;
+		}
+	}
+
+}
+
+public enum MonsterType {
+	SLIME,
+	GOBLIN,
+	TROLL,
+	SKELETON,
+}
