a27e9b526d12ac3b2f9579f1e80b79e493cb05a3
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-12-02 21:36:14 -0200

    Add Trolls to the game

--- Assets/Scripts/SlimeBehaviour.cs
+++ Assets/Scripts/SlimeBehaviour.cs
@@ -7,25 +7,35 @@ public class SlimeBehaviour : MonoBehaviour {
 	private float movementCooldown;
 	private GridMovement _gridMovement;
 
-	private Vector3 _startPosition;
+	public bool upFirst = true;
+	private Vector2 firstMove,lastMove;
+
+	[SerializeField]private Vector3 _startPosition;
 	// Use this for initialization
 	void Start () {
 		_gridMovement = GetComponent<GridMovement> ();
 		movementCooldown = cooldown;
 		_startPosition = this.transform.position;
+		if(upFirst){
+			firstMove = Vector2.up;
+			lastMove = Vector2.down;
+		}else{
+			firstMove = Vector2.down;
+			lastMove = Vector2.up;
+		}
+
 	}
 
 	// Update is called once per frame 
 	void Update () {
-		movementCooldown -= Time.deltaTime;
-		if (movementCooldown <= 0) {
-			
-			if(this.transform.position != _startPosition)
-				_gridMovement.MoveBy (Vector2.up);
-			else
-				_gridMovement.MoveBy (Vector2.down);
-			movementCooldown = cooldown;
+			movementCooldown -= Time.deltaTime;
+			if(movementCooldown <= 0){ 
+				if(MathUtil.IsApproximate(transform.position, _startPosition, .1f))
+					_gridMovement.MoveBy (firstMove); 	 
+				else
+					_gridMovement.MoveBy (lastMove);
+				movementCooldown = cooldown;
+			}
 		}
-	}
 
 }
