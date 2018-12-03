d7559719b50832be18b54b87d66b39ef56d9fc64
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-12-03 00:02:34 -0200

    Adiciona baus e seus comportamentos ; Adiciona itens ; Adiciona inventory ao jogo ; Adiciona Sprites de UI

--- Assets/Scripts/InGameItem.cs
+++ Assets/Scripts/InGameItem.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class InGameItem : MonoBehaviour {
 
-	public Item item;			// Scriptable Object
+	public Item item; // Scriptable Object
 
+	public Sprite chestImagem;
+	public SpriteRenderer chestRenderer;
+
+	public Collider2D chestCollider;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start () {
+		chestRenderer = GetComponent<SpriteRenderer> ();
+	}
 	void Update () {
 
 		/// TODO: Change this behaviour to a collision detection
+		if (chestCollider.OverlapPoint ( Camera.main.ScreenToWorldPoint(Input.mousePosition))) {
+			if (Input.GetMouseButtonDown (1)) {
+				if (InventoryManager.Instance.ItemCollected (item))
+					chestRenderer.sprite = chestImagem;
+				else
+					Debug.Log ("Inventory Full!");
+			}
 
-		if(Input.GetMouseButtonDown(1)){
-			if(InventoryManager.Instance.ItemCollected(item))
-				Destroy(gameObject);
-			else
-				Debug.Log("Inventory Full!");
 		}
-			
 	}
 }
