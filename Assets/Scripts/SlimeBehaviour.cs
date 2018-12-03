366ac6359e7baae9b3113be0483a22f97dee0477
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-12-03 00:22:48 -0200

    Adiciona colisores aos monstros e visuais a UI

--- Assets/Scripts/SlimeBehaviour.cs
+++ Assets/Scripts/SlimeBehaviour.cs
@@ -56,4 +56,16 @@ public class SlimeBehaviour : MonoBehaviour {
 			_gridMovement.MoveBy (lastMove);
 	}
 
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player"){
+			
+		}
+	}
+
 }
