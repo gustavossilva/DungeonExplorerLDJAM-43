f48ac87501cb7d4bbae25bf06fef6c3895ca2976
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-12-03 14:30:47 -0200

    [UPDATE] Boss art fixed. First musics added

---
+++ Assets/MusicHolder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicHolder : Singleton<MusicHolder> {
+
+	public AudioClip menuMusic;
+	public AudioClip dungeonMusic;
+
+	protected override void Awake(){
+		base.IsPersistentBetweenScenes = true;
+		base.Awake();
+
+
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode){
+		if(scene.name == "StartMenu"){
+			PlayMusic(menuMusic, 0.5f);
+		}else if(scene.name == "GameScene"){
+			PlayMusic(dungeonMusic, 0.2f);
+		}
+	}
+
+	private void PlayMusic(AudioClip clip, float volume){
+		AudioSource source = GetComponent<AudioSource>();
+		source.Stop();
+		source.clip = clip;
+		source.loop = true;
+		source.volume = volume;
+		source.Play();
+	}
+
+}
