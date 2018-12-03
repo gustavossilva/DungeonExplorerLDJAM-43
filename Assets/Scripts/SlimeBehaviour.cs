6c7c6ead9dc6795adc58a3ce0371a1cb46833410
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-12-02 22:51:40 -0200

    Adiciona animacoes para slimes e goblins

--- Assets/Scripts/SlimeBehaviour.cs
+++ Assets/Scripts/SlimeBehaviour.cs
@@ -1,41 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Spine.Unity;
 using UnityEngine;
-
 public class SlimeBehaviour : MonoBehaviour {
 	public float cooldown = 0.35f;
 	private float movementCooldown;
 	private GridMovement _gridMovement;
 
 	public bool upFirst = true;
-	private Vector2 firstMove,lastMove;
+	private Vector2 firstMove, lastMove;
 
-	[SerializeField]private Vector3 _startPosition;
+	private SkeletonAnimation slimeSkeleton;
+
+	public AnimationReferenceAsset jump;
+	public AnimationReferenceAsset idle;
+
+	[SerializeField] private Vector3 _startPosition;
 	// Use this for initialization
-	void Start () {
+	private void Awake () {
+		slimeSkeleton = GetComponent<SkeletonAnimation> ();
 		_gridMovement = GetComponent<GridMovement> ();
+	}
+	void Start () {
 		movementCooldown = cooldown;
 		_startPosition = this.transform.position;
-		if(upFirst){
+		if (upFirst) {
 			firstMove = Vector2.up;
 			lastMove = Vector2.down;
-		}else{
+		} else {
 			firstMove = Vector2.down;
 			lastMove = Vector2.up;
 		}
-
+		slimeSkeleton.AnimationState.SetAnimation (0, idle, true);
+		slimeSkeleton.AnimationState.Event += MoveAfterAnimation;
+	}
+	private void MoveAfterAnimation (Spine.TrackEntry track, Spine.Event e) {
+		// When the animation passes through the energy event, starts showing the energy text
+		if (string.Equals ("jump", e.Data.Name, System.StringComparison.Ordinal))
+			Move ();
 	}
 
 	// Update is called once per frame 
 	void Update () {
-			movementCooldown -= Time.deltaTime;
-			if(movementCooldown <= 0){ 
-				if(MathUtil.IsApproximate(transform.position, _startPosition, .1f))
-					_gridMovement.MoveBy (firstMove); 	 
-				else
-					_gridMovement.MoveBy (lastMove);
-				movementCooldown = cooldown;
-			}
+		movementCooldown -= Time.deltaTime;
+		if (movementCooldown <= 0) {
+			movementCooldown = cooldown;
+			slimeSkeleton.AnimationState.SetAnimation (1, jump, false);
 		}
+	}
+
+	void Move () {
+		if (MathUtil.IsApproximate (transform.position, _startPosition, .1f))
+			_gridMovement.MoveBy (firstMove);
+		else
+			_gridMovement.MoveBy (lastMove);
+	}
 
 }
