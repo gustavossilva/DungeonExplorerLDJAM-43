153cbad36b63ea30fb3be6bc2f0e74f5c601ea34
Gustavo Silva <xgustavossilva@gmail.com>
2018-12-03 20:05:51 -0200

    [Add] final scene

---
+++ Assets/Scripts/Managers/FinalManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalManager : MonoBehaviour {
+	
+	void Update () {
+		if(Input.anyKeyDown)
+		{
+			SceneManager.LoadScene("StartMenu");
+		}
+	}
+}
