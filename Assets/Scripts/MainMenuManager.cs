439092a5a66fe8f97456b0b3c4b3bb97840e6445
Gustavo Silva <xgustavossilva@gmail.com>
2018-12-04 21:25:10 -0200

    Fixed tutorial

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,7 +2,19 @@
 using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour {
 
+	public GameObject tutorial;
 
+	private void Start() {
+		if(!PlayerPrefs.HasKey("First")){
+			PlayerPrefs.SetInt("First",0);
+		}
+		if(PlayerPrefs.GetInt("First") == 0)
+		{
+			tutorial.SetActive(true);
+			PlayerPrefs.SetInt("First",1);
+		}
+	}
+	
 	public void StartGame()
 	{
 		SceneManager.LoadScene("GameIntro");
