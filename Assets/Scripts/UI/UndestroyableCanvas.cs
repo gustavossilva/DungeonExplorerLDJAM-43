302ca0dc4bf82661c77ca7bb55e587b0b9627677
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-12-05 14:41:40 -0200

    [UPDATE] Inventory no longer appears during combat

--- Assets/Scripts/UI/UndestroyableCanvas.cs
+++ Assets/Scripts/UI/UndestroyableCanvas.cs
@@ -11,7 +11,7 @@ public class UndestroyableCanvas : Singleton<UndestroyableCanvas> {
 	}
 
 	void OnSceneLoaded(Scene scene, LoadSceneMode mode){
-		if(scene.name == "GameScene"){
+		// if(scene.name == "GameScene"){
 			try{
 				GetComponent<PointerOverUI>()._eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
 			}
@@ -20,25 +20,27 @@ public class UndestroyableCanvas : Singleton<UndestroyableCanvas> {
 			}
 			// Disable the inventory canvas when you are in combat
 			if(scene.name == "Combat" || scene.name == "GameOver" || scene.name == "FinalScene")
-				gameObject.SetActive(false);
+				transform.GetChild(0).gameObject.SetActive(false);
 			else
-				gameObject.SetActive(true);
+				transform.GetChild(0).gameObject.SetActive(true);
 			if(scene.name == "GameOver" || scene.name == "FinalScene")
 			{
 				Destroy(gameObject);
 			}
-		}
+		// }
 	}
 
-protected override void OnEnable() {
+protected override void SubscribeEvents(){
 	SceneManager.sceneLoaded += OnSceneLoaded;
+	base.SubscribeEvents();
 }
 
-protected override void OnDisable() {
-	SceneManager.sceneLoaded -= OnSceneLoaded;
-}
+// protected override void OnDisable() {
+// 	SceneManager.sceneLoaded -= OnSceneLoaded;
+// }
 
-protected override void  OnDestroy() {
+protected override void UnsubscribeEvents() {
 		SceneManager.sceneLoaded -= OnSceneLoaded;
+		base.UnsubscribeEvents();
 	}
 }
